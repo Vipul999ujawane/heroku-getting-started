ac748da67fba253e1554477429e2c593804d8b45
Vipul999ujawane <vipul999ujawane@gmail.com>
2016-08-27 01:34:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ app.get('/reverse/:TEXT',function(req,res){
   res.send(yolo);
 })
 
-app.get('/randompic',function(req,res){
-  res.send(http://loremflickr.com/320/240);
-})
+
 
 app.get('/', function(request, response) {
   response.render('pages/index');
