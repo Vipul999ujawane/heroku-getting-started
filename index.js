0331c10aff74521f150759037f20d34d465e2ae7
Vipul999ujawane <vipul999ujawane@gmail.com>
2016-08-28 16:46:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 
 app.get('/reverse/:TEXT',function(req,res){
   
-  res.send(reverse(req.params.TEXT));
+  res.send(req.params.TEXT.split('').reverse().join(''));
 })
 
 
