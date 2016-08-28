8a5afbc83ad866fcd95f9026573410ce799fbcb5
Vipul999ujawane <vipul999ujawane@gmail.com>
2016-08-28 15:48:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-var request =require('request');
-var bodyParser = require('body-parser')
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
